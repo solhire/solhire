f8a503726e645563ef7f43377a8bbfcd08ed0c1a
solhire <solhire@proton.me>
2025-03-10 08:53:24 -0500

    Remove password protection from site

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -3,7 +3,6 @@
 import Header from './Header';
 import Footer from './Footer';
 import DevelopmentBanner from './DevelopmentBanner';
-import PasswordProtection from '@/components/auth/PasswordProtection';
 
 interface MainLayoutProps {
   children: React.ReactNode;
@@ -11,14 +10,12 @@ interface MainLayoutProps {
 
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
-    <PasswordProtection>
-      <div className="min-h-screen flex flex-col">
-        <DevelopmentBanner />
-        <Header />
-        <main className="flex-1 pt-16 sm:pt-24 md:pt-28">{children}</main>
-        <Footer />
-      </div>
-    </PasswordProtection>
+    <div className="min-h-screen flex flex-col">
+      <DevelopmentBanner />
+      <Header />
+      <main className="flex-1 pt-16 sm:pt-24 md:pt-28">{children}</main>
+      <Footer />
+    </div>
   );
 };
 
