a1bdbd38674e1661b2f9f18a47f236bde5d23b12
solhire <solhire@proton.me>
2025-03-11 01:27:48 -0500

    remove: site-wide password protection

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -3,7 +3,6 @@
 import { ReactNode } from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import PasswordProtection from '../auth/PasswordProtection';
 
 interface MainLayoutProps {
   children: ReactNode;
@@ -11,14 +10,12 @@ interface MainLayoutProps {
 
 export default function MainLayout({ children }: MainLayoutProps) {
   return (
-    <PasswordProtection>
-      <div className="min-h-screen bg-background flex flex-col">
-        <Header />
-        <main className="flex-grow">
-          {children}
-        </main>
-        <Footer />
-      </div>
-    </PasswordProtection>
+    <div className="min-h-screen bg-background flex flex-col">
+      <Header />
+      <main className="flex-grow">
+        {children}
+      </main>
+      <Footer />
+    </div>
   );
 } 
