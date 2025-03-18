e3b80f3af88a4b7c34ecee1795869a735b6493b9
solhire <solhire@gmail.com>
2025-03-18 12:52:46 -0500

    Remove password protection from site

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -1,12 +1,9 @@
 'use client';
 
-import PasswordProtection from '@/components/auth/PasswordProtection';
 import { Providers } from './providers';
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
-    <PasswordProtection>
-      <Providers>{children}</Providers>
-    </PasswordProtection>
+    <Providers>{children}</Providers>
   );
 } 
